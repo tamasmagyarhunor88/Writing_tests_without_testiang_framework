8ddca8c2d5b875642ad1c54cfcb5bd7bca3dc673
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-03-05 16:57:21 +0000

    Refactor, writing nicer tests without testing library

--- test/circle-tests.js
+++ test/circle-tests.js
@@ -1,11 +1,18 @@
-(function(exports) {
-  function testCircleRadiusDefaultsTo10() {
-    var circle = new Circle();
+// (function(exports) {
+//   function testCircleRadiusDefaultsTo10() {
+//     var circle = new Circle();
+//
+//     if (circle.radius !== 10) {
+//       throw new Error("Circle size is not 10");
+//     }
+//   };
+//
+//   testCircleRadiusDefaultsTo10();
+// })(this);
 
-    if (circle.radius !== 10) {
-      throw new Error("Circle size is not 10");
-    }
-  };
+function testCircleRadiusDefaultsTo10() {
+  var circle = new Circle();
+  assert.isTrue(circle.radius === 10);
+};
 
-  testCircleRadiusDefaultsTo10();
-})(this);
+testCircleRadiusDefaultsTo10();
